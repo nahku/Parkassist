2b451cb20fc2f87ceb134c27bd6b6b8ea86ab393
nahku <nsaidy@t-online.de>
2020-03-28 17:19:09 +0100

    fixed D6 and added D3 plots

--- Matlab/D6_script.m
+++ Matlab/D6_script.m
@@ -1,10 +1,42 @@
-set_param('D6','StopTime','10');
-set_param('D6','FixedStep',sprintf('%f',0.01));
-set_param('D6/p','value',sprintf('%f',0.1));
+set_param('D6','StopTime','3');
+set_param('D6','FixedStep',sprintf('%f',0.001));
+set_param('D6/p','value',sprintf('%f',0.04));
 res = sim('D6','SaveOutput','on','SaveState','on');
 
 t = res.tout;
 v = res.yout{1}.Values.Data;
 s = res.yout{2}.Values.Data;
-pulse_hz = res.yout{3}.Values.Data;
+pulse_frequency = res.yout{3}.Values.Data;
+pulse = res.yout{4}.Values.Data;
+
+
+subplot(4,1,1);
+plot(t,v*3.6, 'LineWidth', 1.5);
+set(gca,'FontSize',20)
+title('Velocity of car in full brake');
+xlabel('Time in [s]');
+ylabel('Velocity in [km/h]');
+
+subplot(4,1,2);
+plot(t,s, 'LineWidth', 1.5)
+set(gca,'FontSize',20)
+title('Covered distance');
+xlabel('Time in [s]');
+ylabel('Distance in [m]');
+
+subplot(4,1,3);
+plot(t,pulse_frequency, 'LineWidth', 1.5)
+set(gca,'FontSize',20)
+title('Pulse frequency');
+xlabel('Time in [s]');
+ylabel('Pulse Frequency');
+
+subplot(4,1,4);
+plot(t,pulse, 'LineWidth', 1.5)
+set(gca,'FontSize',20)
+title('Pulse signal');
+xlabel('Time in [s]');
+ylabel('Pulse Signal');
+
+
 
